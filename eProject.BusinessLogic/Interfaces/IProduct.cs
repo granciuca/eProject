67f1399c7911e5654e42cf39d701e8095b2407ec
Adrian Granciuc <granciuca@gmail.com>
2020-04-28 13:51:21 +0300

    modified

---
+++ eProject.BusinessLogic/Interfaces/IProduct.cs
@@ -0,0 +1,15 @@
+﻿using eProject.Domain.Entities.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eProject.BusinessLogic.Interfaces
+{
+    public interface IProduct
+    {
+        List<string> GetProductsList();
+        ProductsResp SetProductsList(ProductsDatas prod);
+    }
+}
---
+++ eProject.BusinessLogic/ProductsBL.cs
@@ -0,0 +1,24 @@
+﻿using eProject.BusinessLogic.Core;
+using eProject.BusinessLogic.Interfaces;
+using eProject.Domain.Entities.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eProject.BusinessLogic
+{
+    public class ProductsBL : UserApi, IProduct
+    {
+        public List<string> GetProductsList()
+        {
+            return ProductListLogic();
+        }
+
+        public ProductsResp SetProductsList(ProductsDatas prod)
+        {
+            return AddProducts(prod);
+        }
+    }
+}
